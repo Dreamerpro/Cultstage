c8bc67b35f2decfdbcd7b16abb248bffd7b007c4
lookplex <user.lookplex@gmail.com>
2015-10-23 16:43:46 +0530

    firs

---
+++ resources/views/index.blade.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="{{css/all.css}}">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<style type="text/css">
+
+</style>
+</head>
+<body>
+<div class="page-header">
+	<div class="sign-in-form pull-right">
+		<div class="form-inline">
+		<div class="form-group"><input class='form-control' type="text" placeholder="Username"></div>
+		<div class="form-group"><input class='form-control' type="password" placeholder="Password"></div>
+		<div class="form-group"><input type="button" class="btn btn-warning" value="Sign In"></div>
+		</div>
+	</div>
+	<a href="index.html" class="logo"><img src="assets/image/logo.PNG"><h4>Join the Cult. Find your Stage.</h4></a>
+</div>
+<div class="jumbotron clearfix">
+	<div class="">
+	<div class="col-md-6">
+		<form class="signup-form col-md-6">
+			<h3 class="su-head">Sign Up<br><small>Join the Cult.Its Free.</small></h3>
+		<div class="form-inline">
+			<div class="form-group col-50">
+			<input type="text" class="form-control" placeholder="Firstname">
+			</div><div class="form-group col-50">
+			<input type="text" class="form-control" placeholder="Lastname">
+			</div>
+		</div>
+		<div class="form-group">
+			<input type="text" class="form-control" placeholder="Email or Phone no.">
+		</div>
+		<div class="form-group">
+			<input type="text" class="form-control" placeholder="Password">
+		</div>
+		<div class="form-inline">
+			<div class="form-group col-50">
+			<input type="date" class="form-control" placeholder="DOB">
+			</div><div class="form-group col-50">
+			<select class="form-control">
+				<option disabled>Gender</option>
+				<option >Male</option>
+				<option >Female</option>
+			</select>
+			</div>
+		</div>
+		<button class="btn btn-default">Sign Up</button>
+		</form>
+		<div class="col-md-6 social-plugin">
+		<center>OR</center>
+		<button class="btn fb"><i class="fa fa-facebook pull-left"></i>Sign In with Facebook</button>
+		<button class="btn g"><i class="fa fa-google-plus pull-left"></i>Sign In with Gmail</button>
+		</div>
+	</div>
+	<div class="col-md-6 h-c-right">
+		<div class="green-rect">
+			<div class="row-c"><div >Network</div><div >Showcase</div></div>
+			<div class="black-rect"></div>
+			<div class="row-c"><div >Opportunities</div><div >Learn</div></div>
+		</div>
+	</div>
+</div>
+</div>
+
+<div class="page-footer">
+<nav class="footer-nav"><ul><li><a href="#">About</a></li><li><a  href="#">Help</a></li><li><a href="#">Contact Us</a></li><li><a href="#">Language</a></li><li><a href="#">Privacy</a></li><li><a href="#">Feedback</a></li></ul></nav>
+</div>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/theme-a.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<style type="text/css">
+
+</style>
+</head>
+<body>
+<div class="page-header">
+	<div class="pull-right">
+		<div class="top-menu"><a href="#">Home</a><a href="#">Messages</a><a href="#">People</a><a href="#">Notifications</a></div>
+		<div class="search-box form-inline">
+				<div class="form-group"><input class="btn btn-default" type="button" value="Search"></div><div class="form-group"><input class="form-control" type="text"></div>
+		</div>
+	</div>
+	<a class="logo"><h1>CULTSTAGE</h1></a>
+</div>
+
+<div class="jumbotron profile">
+<ul class="nav nav-pills profile-top-menu">
+<li role="presentation" class="active"><a href="#">Mayank's Stage</a></li>
+<li role="presentation" ><a href="#">Opportunities</a></li>
+<li role="presentation" ><a href="#">Meet ups</a></li>
+<li role="presentation" ><a href="#">Forum</a></li>
+<li role="presentation" ><a href="#">Learn</a></li>
+<li role="presentation" ><a href="#">Magazine</a></li>
+<li role="presentation" ><a href="#">Blog</a></li>
+</ul>
+</div>
+<div>
+<ul class="nav nav-tabs">
+<li role="presentation" class="active"><a href="#">Mayank's Stage</a></li>
+<li role="presentation" ><a href="#">Opportunities</a></li>
+<li role="presentation" ><a href="#">Meet ups</a></li>
+<li role="presentation" ><a href="#">Forum</a></li>
+<li role="presentation" ><a href="#">Learn</a></li>
+<li role="presentation" ><a href="#">Magazine</a></li>
+<li role="presentation" ><a href="#">Blog</a></li>
+</ul>
+<div class="profile-detail clearfix">
+<div class="col-md-8">
+	<div class="media">
+	    <div class="media-left">
+	      <a href="#">
+	        <img class="media-object" src="image/pro.jpg" alt="...">
+	      </a>
+	    </div>
+	    <div class="media-body ">
+	      <h4 class="media-heading">Media heading</h4>
+	      <p><i class="t1"></i>Nickelback is a Canadian rock band formed in 1995 in Hanna, Alberta. The band is composed of guitarist and lead vocalist Chad Kroeger, guitarist, keyboardist and backing vocalist Ryan Peake, bassist Mike Kroeger, and drummer Daniel Adair. The band went through a few drummer changes between 1995 and 2005, achieving its current form when Adair replaced drummer Ryan Vikedal.<br>
+
+<i class="t1"></i>Nickelback is one of the most commercially successful Canadian groups, having sold more than 50 million albums worldwide[2] and ranking as the eleventh best-selling music act, and the second best-selling foreign act in the U.S. of the 2000s, behind The Beatles.[3][4] Billboard ranks them the most successful rock group of the decade; their song "How You Remind Me" was listed as the best-selling rock song of the decade and the fourth best-selling of the decade. They were listed number seven on the Billboard top artist of the decade, with four albums listed on the Billboard top albums of the decade.[5]</p>
+	    </div>
+	 </div>
+	<div class="geo">
+		<!-- <input class="form-control" type="text">
+		<ul>
+			<li>Occupation..</li>
+			<li><strong>Film & Television</strong></li>
+		</ul> -->
+		<select>
+			<option>Occupation.</option>
+			<option><strong>Film & Television</strong></option>
+			<option>Accounting Clerk</option>
+			<option>Acting Teacher</option>
+			<option>Actor</option>
+			<option>Agency Assistant</option>
+			<option>Agent</option>
+			<option>Animal Trainer</option>
+			<option>Animator</option>
+			<option>Art Department Coordinator</option>
+			<option>Art Director</option>
+			<option>Assistant Director</option>
+			<option>Assistant Production Office Coordinator</option>
+			<option>Best Boy Electric</option>
+			<option>Best Boy Grip</option>
+			<option>Boom Operator</option>
+		</select>
+	<div id="googleMap" style="width:100%; height:380px" ></div>
+	</div>
+</div>
+<div class="col-md-4">
+	<ul class="media-list right-bar">
+	  <li class="media">
+	    <div class="media-left">
+	      <a href="#">
+	        <img class="media-object" src="image/mm.png" alt="...">
+			</a>
+	      <small>30 days ago</small>
+	      <a href="#"><small>[+] Expand details.</small></a>
+	    </div>
+	    <div class="media-body">
+	    	<div class="pull-right">
+	    		<input type="button" class="btn btn-primary" value="Submit Now">
+	    		<p><strong>Ends Today</strong><br><small>FREE</small></p>
+	    	</div>
+	      <h5 class="media-heading"><strong>Feature your video on Making Music Magazines website.</strong> </h5>
+	      <p><small><strong>Location</strong> <i class="t1"></i>Online</small> </p>
+	    </div>
+	  </li>
+	  <li class="media">
+	    <div class="media-left">
+	      <a href="#">
+	        <img class="media-object" src="image/ar.jpg" alt="...">
+			</a>
+	      <small>48 days ago</small>
+	      <a href="#"><small>[+] Expand details.</small></a>
+	    </div>
+	    <div class="media-body">
+	    	<div class="pull-right">
+	    		<input type="button" class="btn btn-primary" value="Submit Now">
+	    		<p><strong>Ends Today</strong><br><small>FREE</small></p>
+	    	</div>
+	      <h5 class="media-heading"><strong>Reach thousand of Americana Fans in Americana Rythm Music Magazine.</strong> </h5>
+	      <p><small><strong>Location</strong> <i class="t1"></i>Bridgewater VA</small> </p>
+	    </div>
+	  </li>
+	  <li class="media">
+	    <div class="media-left">
+	      <a href="#">
+	        <img class="media-object" src="image/ai.jpg" alt="...">
+			</a>
+	      <small>19 days ago</small>
+	      <a href="#"><small>[+] Expand details.</small></a>
+	    </div>
+	    <div class="media-body">
+	    	<div class="pull-right">
+	    		<input type="button" class="btn btn-primary" value="Submit Now">
+	    		<p><strong>2 Days</strong><br><small>FREE</small></p>
+	    	</div>
+	      <h5 class="media-heading"><strong>Jump the audition for American Idol Season XV.</strong> </h5>
+	      <p><small><strong>Location</strong> <i class="t1"></i>Online</small> </p>
+	      <p><small><strong>Dates</strong> <i class="t1"></i>Multiple dates available.</small> </p>
+	    </div>
+	  </li>
+	  <li class="media">
+	    <div class="media-left">
+	      <a href="#">
+	        <img class="media-object" src="image/au.png" alt="...">
+			</a>
+	      <small>28 days ago</small>
+	      <a href="#"><small>[+] Expand details.</small></a>
+	    </div>
+	    <div class="media-body">
+	    	<div class="pull-right">
+	    		<input type="button" class="btn btn-primary" value="Submit Now">
+	    		<p><strong>2 Days</strong><br><small>FREE</small></p>
+	    	</div>
+	      <h5 class="media-heading"><strong>Australian Artist: Get Interviewed on Sydney's The AU Review Blog</strong> </h5>
+	      <p><small><strong>Location</strong> <i class="t1"></i>Sydney, NSW, AU</small> </p>
+	    </div>
+	  </li>
+	</ul>
+</div>
+</div>
+</div>
+
+
+<div class="page-footer">
+<nav class="footer-nav"><ul><li><a href="#">About</a></li><li><a  href="#">Help</a></li><li><a href="#">Contact Us</a></li><li><a href="#">Language</a></li><li><a href="#">Privacy</a></li><li><a href="#">Feedback</a></li></ul></nav>
+</div>
+</body>
+<script type='text/javascript' src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBkRzmfedoJ2knAFsyxdhH5AuW41ljbu6E&sensor=false'></script>
+<script type="text/javascript">
+ 
+ function initialize(e,f){
+      e=28.5739334;
+        f=77.2765164;
+      var pos=new google.maps.LatLng(e,f);
+      var mapProp = {
+        center: pos,
+        zoom:14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+      var marker = new google.maps.Marker({
+          position: pos,
+          map: map,
+          draggable:true,
+          animation: google.maps.Animation.DROP,
+          title: 'the place'
+      });
+      marker.setMap(map);    
+      google.maps.event.addListener(marker,'dragend',function(){
+       
+      });
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+</html>
